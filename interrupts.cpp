624c53aa609c00c7ebd4c86cec4725065a4c12ce
mehmetprogramci <99365371+mehmetprogramci@users.noreply.github.com>
2022-06-03 21:24:47 +0300

    Add files via upload

--- interrupts.cpp
+++ interrupts.cpp
@@ -4,7 +4,33 @@
 
 void printf(char* str);
 
+
+
+InterruptHandler::InterruptHandler(uint8_t interrruptNumber, InterruptManager* interruptManager)
+{
+  this->interruptNumber = interruptNumber;
+  this->interruptManager = interruptManager;
+  interruptManager->handlers[interruptNumber] = this;
+}
+InterruptHandler::~InterruptHandler()
+{
+  if(interruptManager->handlers[interruptNumber] == this)
+     interruptManager->handlers[interruptNumber] = 0;
+}
+uint32_t InterruptHandler::HandleInterrupt(uint32_t esp)
+{
+  return esp;
+}
+
+
+
+
+
+
+
 InterruptManager::GateDescriptor InterruptManager::interruptDescriptorTable[256];
+
+InterruptManager* InterruptManager::ActiveInterruptManager = 0;
 
 void InterruptManager::SetInterruptDescriptorTableEntry(
   uint8_t interruptNumber,
@@ -34,8 +60,11 @@ InterruptManager::InterruptManager(GlobalDescriptorTable* gdt)
   const uint8_t IDT_INTERRUPT_GATE = 0xE;
 
   for(uint16_t i = 0; i < 256; i++)
+  {
+     handlers[i] = 0;
      SetInterruptDescriptorTableEntry(i, CodeSegment, &IgnoreInterruptRequest, 0, IDT_INTERRUPT_GATE);
-
+  }
+  
     SetInterruptDescriptorTableEntry(0x20, CodeSegment, &HandleInterruptRequest0x00, 0, IDT_INTERRUPT_GATE);
     SetInterruptDescriptorTableEntry(0x21, CodeSegment, &HandleInterruptRequest0x01, 0, IDT_INTERRUPT_GATE);
 
@@ -68,14 +97,54 @@ InterruptManager::~InterruptManager()
 
 void InterruptManager::Activate()
 {
+    if(ActiveInterruptManager != 0)
+       ActiveInterruptManager->Deactivate();
+    ActiveInterruptManager = this;
     asm("sti");
+}
+
+
+void InterruptManager::Deactivate()
+{
+    if(ActiveInterruptManager == this)
+    {
+       ActiveInterruptManager = 0;
+       asm("cli");
+    }
 }
 
 
 uint32_t InterruptManager::handleInterrupt(uint8_t interruptNumber, uint32_t esp)
 {
+    if(ActiveInterruptManager != 0)
+    return ActiveInterruptManager->DoHandleInterrupt(interruptNumber, esp);
+    return esp;
+}
 
-    printf(" INTERRUPT");
 
+uint32_t InterruptManager::DoHandleInterrupt(uint8_t interruptNumber, uint32_t esp)
+{
+  
+    if(handlers[interruptNumber] != 0)
+    {
+      esp = handlers[interruptNumber]->HandleInterrupt(esp);
+    }
+
+    else if(interruptNumber != 0x20)
+    {
+        char* foo = "UNHADLED INTERRUPT 0x00";
+	char* hex = "012345679ABCDEF";
+	foo[22] = hex[(interruptNumber >> 4) & 0x0F];
+	foo[23] = hex[interruptNumber & 0x0F];
+        printf("foo");
+    }
+
+    if(0x20 <= interruptNumber && interruptNumber < 0x30)
+    {
+      picMasterCommand.Write(0x20);
+      if(0x28 <= interruptNumber)
+          picSlaveCommand.Write(0x20);
+    }
+  
     return esp;
 }
--- interrupts.h
+++ interrupts.h
@@ -7,10 +7,29 @@
 #include "gdt.h"
 
 
+    class InterruptManager;
+
+
+    class InterruptHandler
+    {
+    protected:
+    uint8_t interruptNumber;
+    InterruptManager* interruptManager;
+  
+    InterruptHandler(uint8_t interrruptNumber, InterruptManager* interruptManager);
+    ~InterruptHandler();
+    public:
+    uint32_t HandleInterrupt(uint32_t esp);
+    };
+
+
 class InterruptManager
 {
-
+  friend class InterruptHandler;
   protected:
+
+  static InterruptManager* ActiveInterruptManager;
+  InterruptHandler* handlers[256];
 
   struct GateDescriptor
   {
@@ -50,8 +69,10 @@ InterruptManager(GlobalDescriptorTable* gdt);
 ~InterruptManager();
 
 void Activate();
+void Deactivate();
  
   static uint32_t handleInterrupt(uint8_t interruptNumber, uint32_t esp);
+  uint32_t DoHandleInterrupt(uint8_t interruptNumber, uint32_t esp);
 
   static void IgnoreInterruptRequest();
   static void HandleInterruptRequest0x00();
