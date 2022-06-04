af7e89c3d3438f3aa51787da4e01c17ee1da0fd5
mehmetprogramci <99365371+mehmetprogramci@users.noreply.github.com>
2022-06-04 11:17:18 +0300

    Add files via upload

--- kernel.cpp
+++ kernel.cpp
@@ -2,6 +2,7 @@
 #include "types.h"
 #include "gdt.h"
 #include "interrupts.h"
+#include "keyboard.h"
 
 
 void printf(char* str)
@@ -48,6 +49,17 @@ void printf(char* str)
        
 }
 
+
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+extern "C" void callConstructors()
+{
+  
+  for(constructor* i = &start_ctors; i != &end_ctors; i++)
+  (*i)();
+}
+
 extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot_magic*/)
 {
     printf("Kapilar Onyukleyicisine Hosgeldiniz...");
@@ -55,6 +67,8 @@ extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot
     
         GlobalDescriptorTable gdt;
 	InterruptManager interrupts(&gdt);
+	
+	KeyboardDriver keyboard(&interrupts);
 	
 	interrupts.Activate();
     
